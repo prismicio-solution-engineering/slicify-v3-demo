d1e7108110ba063e513e7ef9e5c887e057e944e9
Dmsooja <dev@sooja.fr>
2026-03-03 09:57:44 +0100

    Update Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests",
   timeout: 30000,
+  reporter: [
+    ["list"],
+    ["html", { outputFolder: "playwright-report", open: "never" }],
+  ],
   use: {
     headless: true,
   },
